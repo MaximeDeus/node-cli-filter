45efdecb91923b1dab895d0561ebb41bf7e5d54e
Maxime Deus <maxime.deroissart@mail.com>
2023-12-20 17:59:54 +0100

    #7 test: add TU for option

--- __tests__/utils/option.test.js
+++ __tests__/utils/option.test.js
@@ -1,11 +1,45 @@
-test('sample OK test', () => {
-    expect.assertions(2);
-    expect(true).toBeTruthy();
-    expect(false).toBeFalsy();
+const InvalidOptionException = require("../../errors/InvalidOptionException");
+const {isValidOption, formatOption, formatOptions} = require("../../utils/option");
+const UnknownOptionException = require("../../errors/UnknownOptionException");
+describe('isValidOption', () => {
+    test('no well formatted option must throw InvalidOptionException', () => {
+        let invalidOption;
+        invalidOption = "help"; // missing '--'
+        expect(() => isValidOption(invalidOption)).toThrow(InvalidOptionException);
+        invalidOption = "-help"; // missing '-'
+        expect(() => isValidOption(invalidOption)).toThrow(InvalidOptionException);
+        invalidOption = "--help="; // missing value behind '='
+        expect(() => isValidOption(invalidOption)).toThrow(InvalidOptionException);
+    });
+
+    test('Unknown option must throw UnknownOptionException', () => {
+        const unknownOption = "--tototititutu";
+        expect(() => isValidOption(unknownOption)).toThrow(UnknownOptionException);
+    });
+
+    test('valid option must return true', () => {
+        const validOption = "--help";
+        expect(isValidOption(validOption)).toBeTruthy();
+    });
 });
 
-test('sample KO test', () => {
-    expect.assertions(3);
-    expect(true).toBeTruthy();
-    expect(false).toBeFalsy();
+describe('formatOption', () => {
+    test('must return Object containing paired optionName/optionValue', () => {
+        const option = "--filter=ry";
+        const formattedOption = formatOption(option);
+        expect(formattedOption).toEqual({filter: "ry"});
+    });
+    test('optionValue must be set as "null" if not provided', () => {
+        const option = "--count";
+        const formattedOption = formatOption(option);
+        expect(formattedOption).toEqual({count: null});
+    });
+});
+
+describe('formatOptions', () => {
+    test('must handle multiple options', () => {
+        const options = ['--filter=ry','--count'];
+        const formattedOptions = formatOptions(options);
+        expect(formattedOptions).toEqual({filter: "ry", count: null});
+    });
 });
