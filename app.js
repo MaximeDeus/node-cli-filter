98cc1bd039b1dd440bd0805a002cba0fa772b37e
Maxime Deus <maxime.deroissart@mail.com>
2023-12-21 14:29:12 +0100

    #12 (WIP) feat: update app.js, now call filterByAnimals when running script instead of displaying option. Must replace static raw datas (i.e 'ry') by dynamic user arguments

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const {formatOptions} = require("./utils/option");
 const options = formatOptions(process.argv.slice(2)); // slice(2) for removing node.exe and app.js filepath from args
+const {data} = require("./data");
+const {filterByAnimals} = require("./utils/filter");
 
-console.log(options);
+const result = filterByAnimals(data,'ry')
+console.log(result);
+
