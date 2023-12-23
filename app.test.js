3cbb432c0c903aebe35386046a1eef05916292b2
Maxime Deus <maxime.deroissart@mail.com>
2023-12-23 22:24:18 +0100

    #18 feat/test: update app.js, handle help option, also update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,12 +1,23 @@
+let logSpy;
+
+let mockHelp;
 let mockFilter;
 let mockCount;
+
 jest.mock('./data/sampleData', () => ({
     data: {data: 'original'},
 }));
 beforeEach(() => {
-    jest.resetModules();
+    logSpy = jest.spyOn(console, 'log');
+    logSpy.mockImplementation(() => {}); // avoid displaying message
+
+    mockHelp = require('./utils/help');
     mockFilter = require('./utils/filter');
     mockCount = require('./utils/count');
+
+    jest.mock('./utils/help', () => ({
+        showHelp: jest.fn(),
+    }));
     jest.mock('./utils/filter', () => ({
         filterByAnimals: jest.fn(() => ({data: 'filtered'})),
     }));
@@ -15,14 +26,48 @@ beforeEach(() => {
     }));
 });
 
-test('no option must call nothing', () => {
+afterEach(() => {
+    logSpy.mockRestore();
+    jest.resetModules();
+})
+
+test('no option still have to call console.log', () => {
     jest.mock('./utils/option', () => ({
         formatOptions: jest.fn(() => ({})),
     }));
 
-    expect(mockFilter.filterByAnimals).toHaveBeenCalledTimes(0);
+    expect(logSpy).toHaveBeenCalledTimes(0);
     require('./app');
+    expect(logSpy).toHaveBeenCalledTimes(1);
+});
+
+test('help option activated must call showHelp once', () => {
+    jest.mock('./utils/option', () => ({
+        formatOptions: jest.fn(() => ({'help' : null})),
+    }));
+
+    expect(mockHelp.showHelp).toHaveBeenCalledTimes(0);
+    require('./app');
+    expect(mockHelp.showHelp).toHaveBeenCalledTimes(1);
+});
+test('help option activated must not call console.log', () => {
+    jest.mock('./utils/option', () => ({
+        formatOptions: jest.fn(() => ({'help' : null})),
+    }));
+
+    expect(logSpy).toHaveBeenCalledTimes(0);
+    require('./app');
+    expect(logSpy).toHaveBeenCalledTimes(0);
+});
+test('help option activated must not call filter nor count', () => {
+    jest.mock('./utils/option', () => ({
+        formatOptions: jest.fn(() => ({'help' : null,'filter' : null,'count' : null})),
+    }));
+
+    require('./app');
+    expect(mockHelp.showHelp).toHaveBeenCalledTimes(1);
     expect(mockFilter.filterByAnimals).toHaveBeenCalledTimes(0);
+    expect(mockCount.count).toHaveBeenCalledTimes(0);
 });
 test('filter option activated must call filterByAnimals once', () => {
     jest.mock('./utils/option', () => ({
