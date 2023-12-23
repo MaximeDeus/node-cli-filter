5ab61b375ce095566d4cb5a4b095358ddcbfa36e
Maxime Deus <maxime.deroissart@mail.com>
2023-12-23 22:24:18 +0100

    #18 feat/test: update app.js, handle help option, also update app.test.js

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const {formatOptions} = require("./utils/option");
 const options = formatOptions(process.argv.slice(2)); // slice(2) for removing node.exe and app.js filepath from args
 const {data} = require("./data/sampleData");
+const {showHelp} = require("./utils/help");
 const {filterByAnimals} = require("./utils/filter");
 const {count} = require("./utils/count");
 
 let result = data;
-if ('filter' in options) {
-    result = filterByAnimals(data,options.filter);
+if ('help' in options) {
+    showHelp();
+} else {
+    if ('filter' in options) {
+        result = filterByAnimals(data, options.filter);
+    }
+    if ('count' in options) {
+        result = count(result);
+    }
+    console.log(JSON.stringify(result, null, 2)); // pretty printing;
 }
-if ('count' in options) {
-    result = count(result);
-}
-console.log(JSON.stringify(result, null, 2)); // pretty printing;
-
