4950041da2e35cf149d6eba4f31ba335d5832465
Maxime Deus <maxime.deroissart@mail.com>
2023-12-23 15:41:45 +0100

    #16 test: update app.test.js, add count's case

--- app.test.js
+++ app.test.js
@@ -1,13 +1,17 @@
-// TODO add test with count
 let mockFilter;
+let mockCount;
 jest.mock('./data/sampleData', () => ({
     data: {data: 'original'},
 }));
 beforeEach(() => {
     jest.resetModules();
     mockFilter = require('./utils/filter');
+    mockCount = require('./utils/count');
     jest.mock('./utils/filter', () => ({
         filterByAnimals: jest.fn(() => ({data: 'filtered'})),
+    }));
+    jest.mock('./utils/count', () => ({
+        count: jest.fn(() => ({data: 'counted'})),
     }));
 });
 
@@ -28,4 +32,23 @@ test('filter option activated must call filterByAnimals once', () => {
     expect(mockFilter.filterByAnimals).toHaveBeenCalledTimes(0);
     require('./app');
     expect(mockFilter.filterByAnimals).toHaveBeenCalledTimes(1);
+});
+
+test('count option activated must call count once', () => {
+    jest.mock('./utils/option', () => ({
+        formatOptions: jest.fn(() => ({'count' : null})),
+    }));
+
+    expect(mockCount.count).toHaveBeenCalledTimes(0);
+    require('./app');
+    expect(mockCount.count).toHaveBeenCalledTimes(1);
+});
+
+test('filter and count option activated must call both', () => {
+    jest.mock('./utils/option', () => ({
+        formatOptions: jest.fn(() => ({'filter': null, 'count' : null})),
+    }));
+    require('./app');
+    expect(mockFilter.filterByAnimals).toHaveBeenCalledTimes(1);
+    expect(mockCount.count).toHaveBeenCalledTimes(1);
 });
