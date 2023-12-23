01b7c5a1fc1c4a2ec648ce3cd744b5dfb037108f
Maxime Deus <maxime.deroissart@mail.com>
2023-12-23 22:25:52 +0100

    #18 refacto: update help.test.js, now init and reset spyOn mock on beforeEach and afterEach lifecycle's functions

--- utils/help.test.js
+++ utils/help.test.js
@@ -1,12 +1,17 @@
 const { showHelp } = require('./help');
+let logSpy;
+
+beforeEach(() => {
+    logSpy = jest.spyOn(console, 'log');
+    logSpy.mockImplementation(() => {}); // avoid displaying message
+});
+
+afterEach(() => {
+    logSpy.mockRestore();
+})
 
 test('showHelp must call console.log', () => {
-    const logSpy = jest.spyOn(console, 'log');
-    logSpy.mockImplementation(() => {}); // avoid displaying message
-
     expect(logSpy).toHaveBeenCalledTimes(0);
     showHelp();
     expect(logSpy).toHaveBeenCalledTimes(1);
-
-    logSpy.mockRestore(); // restore console.log behavior
 });
